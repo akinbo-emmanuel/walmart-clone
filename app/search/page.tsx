b4b96c8fd58fb9db61e1eaee786663ac01d9c0a6
Emmanuel Akinbo <59098948+akinbo-emmanuel@users.noreply.github.com>
2024-02-22 15:13:49 +0100

    Search Page

---
+++ app/search/page.tsx
@@ -0,0 +1,14 @@
+type Props ={
+    searchParams:{
+        q: string;
+    }
+}
+
+const SearchPage = ({searchParams: { q }}: Props) => {
+    // Fetch the Search Results
+    return (
+        <div>SearchPage</div>
+    )
+}
+
+export default SearchPage
